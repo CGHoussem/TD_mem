d0772cf096111d642e8d1480b4541acce159a5cc
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2021-01-30 17:34:47 +0100

    added copy benchmark

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -22,7 +22,7 @@ if [ ! -d "system" ]; then
 	mkdir system system/cpu system/caches system/memory
 else
 	if [ ! -d "system/cpu" ]; then
-		mkdir system/cpy
+		mkdir system/cpu
 	fi
 	if [ ! -d "system/caches" ]; then
 		mkdir system/caches
@@ -45,63 +45,147 @@ cat /sys/devices/system/cpu/cpu*/cache/index*/* > system/caches/all.txt
 # Populating the memory directory
 cat /proc/meminfo > system/memory/info.txt
 
-# ==================================
-# ========= load benchmark =========
-# ==================================
-# Building the "load" benchmark binary
-cd load
-make clean
-make
-# Running "load" benchmark on 128 KiB of memoy (fits in L1 cache)
-taskset -c 1 ./load_SSE_AVX $(( 128 * 2**10 )) 1000 | cut -d';' -f1,9 > load_L1.dat
-# Running "load" benchmark on 4 MiB of memory (fits in L2 cache)
-taskset -c 2 ./load_SSE_AVX $(( 4 * 2**20 )) 500 | cut -d';' -f1,9 > load_L2.dat
-# Running "load" benchmark on 16 MiB of memory (fits in L3 cache)
-taskset -c 3 ./load_SSE_AVX $(( 16 * 2**20 )) 100 | cut -d';' -f1,9 > load_L3.dat
-# Running "load" benchmark on 1 GiB of memory (fits in DRAM)
-taskset -c 4 ./load_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > load_DRAM.dat
-# Drawing the load benchmark plot
-cd ..
-gnuplot -c "plot_load_bw.gp" > load_bw.png
+function load_benchmark() {
+	# ==================================
+	# ========= load benchmark =========
+	# ==================================
+	# Building the "load" benchmark binary
+	cd load
+	make clean
+	make
+	# Running "load" benchmark on 128 KiB of memoy (fits in L1 cache)
+	taskset -c 1 ./load_SSE_AVX $(( 128 * 2**10 )) 1000 | cut -d';' -f1,9 > load_L1.dat
+	# Running "load" benchmark on 4 MiB of memory (fits in L2 cache)
+	taskset -c 2 ./load_SSE_AVX $(( 4 * 2**20 )) 500 | cut -d';' -f1,9 > load_L2.dat
+	# Running "load" benchmark on 16 MiB of memory (fits in L3 cache)
+	taskset -c 3 ./load_SSE_AVX $(( 16 * 2**20 )) 100 | cut -d';' -f1,9 > load_L3.dat
+	# Running "load" benchmark on 1 GiB of memory (fits in DRAM)
+	taskset -c 4 ./load_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > load_DRAM.dat
+	# Drawing the load benchmark plot
+	cd ..
+	gnuplot -c "plot_load_bw.gp" > load_bw.png
+}
 
-# ==================================
-# ======== store benchmark =========
-# ==================================
-# Building the "store" benchmark binary
-cd store/
-make clean
-make
-# Running "store" benchmark on 128 KiB of memoy (fits in L1 cache)
-taskset -c 1 ./store_SSE_AVX $(( 128 * 2**10 )) 1000 | cut -d';' -f1,9 > store_L1.dat
-# Running "store" benchmark on 4 MiB of memoy (fits in L2 cache)
-taskset -c 2 ./store_SSE_AVX $(( 4 * 2**20 )) 500 | cut -d';' -f1,9 > store_L2.dat
-# Running "store" benchmark on 16 MiB of memoy (fits in L3 cache)
-taskset -c 3 ./store_SSE_AVX $(( 16 * 2**20 )) 100 | cut -d';' -f1,9 > store_L3.dat
-# Running "store" benchmark on 1 GiB of memoy (fits in DRAM)
-taskset -c 4 ./store_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > store_DRAM.dat
-# Drawing the store benchmark plot
-cd ..
-gnuplot -c "plot_store_bw.gp" > store_bw.png
+function store_benchmark() {
+	# ==================================
+	# ======== store benchmark =========
+	# ==================================
+	# Building the "store" benchmark binary
+	cd store/
+	make clean
+	make
+	# Running "store" benchmark on 128 KiB of memoy (fits in L1 cache)
+	taskset -c 1 ./store_SSE_AVX $(( 128 * 2**10 )) 1000 | cut -d';' -f1,9 > store_L1.dat
+	# Running "store" benchmark on 4 MiB of memoy (fits in L2 cache)
+	taskset -c 2 ./store_SSE_AVX $(( 4 * 2**20 )) 500 | cut -d';' -f1,9 > store_L2.dat
+	# Running "store" benchmark on 16 MiB of memoy (fits in L3 cache)
+	taskset -c 3 ./store_SSE_AVX $(( 16 * 2**20 )) 100 | cut -d';' -f1,9 > store_L3.dat
+	# Running "store" benchmark on 1 GiB of memoy (fits in DRAM)
+	taskset -c 4 ./store_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > store_DRAM.dat
+	# Drawing the store benchmark plot
+	cd ..
+	gnuplot -c "plot_store_bw.gp" > store_bw.png
+}
+
+function ntstore_benchmark() {
+	# ==================================
+	# ======= ntstore benchmark ========
+	# ==================================
+	# Building the "ntstore" benchmark binary
+	cd ntstore/
+	make clean
+	make
+	# Running "ntstore" benchmark on 1 GiB of memoy
+	taskset -c 1 ./ntstore_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > ntstore_DRAM_1.dat
+	# Running "ntstore" benchmark on 2 GiB of memoy
+	taskset -c 2 ./ntstore_SSE_AVX $(( 2 * 2**30 )) 10 | cut -d';' -f1,9 > ntstore_DRAM_2.dat
+	# Drawing the ntstore benchmark plot
+	cd ..
+	gnuplot -c "plot_nstore_bw.gp" > ntstore_bw.png
+}
+
+function copy_benchmark() {
+	# ==================================
+	# ======== copy benchmark =========
+	# ==================================
+	# Building the "copy" benchmark binary
+	cd copy/
+	make clean
+	make
+	# Running "copy" benchmark on 128 KiB of memoy (fits in L1 cache)
+	taskset -c 1 ./copy_SSE_AVX $(( 128 * 2**10 )) 1000 | cut -d';' -f1,9 > copy_L1.dat
+	# Running "copy" benchmark on 4 MiB of memoy (fits in L2 cache)
+	taskset -c 2 ./copy_SSE_AVX $(( 4 * 2**20 )) 500 | cut -d';' -f1,9 > copy_L2.dat
+	# Running "copy" benchmark on 16 MiB of memoy (fits in L3 cache)
+	taskset -c 3 ./copy_SSE_AVX $(( 16 * 2**20 )) 100 | cut -d';' -f1,9 > copy_L3.dat
+	# Running "copy" benchmark on 1 GiB of memoy (fits in DRAM)
+	taskset -c 4 ./copy_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > copy_DRAM.dat
+	# Drawing the copy benchmark plot
+	cd ..
+	gnuplot -c "plot_copy_bw.gp" > copy_bw.png
+}
+
+function memcpy_benchmark() {
+	# TODO: memcpy benchmark
+	:
+}
+
+function pc_benchmark() {
+	# TODO: pc benchmark
+	:
+}
+
+function reduc_benchmark() {
+	# TODO: reduc benchmark
+	:
+}
+
+function dp_benchmark() {
+	# TODO: dotprod benchmark
+	:
+}
+
+function triad_benchmark() {
+	# TODO: triad benchmark
+	:
+}
 
 
-# ==================================
-# ======= ntstore benchmark ========
-# ==================================
-# Building the "ntstore" benchmark binary
-cd ntstore/
-make clean
-make
-# Running "ntstore" benchmark on 1 GiB of memoy
-taskset -c 1 ./ntstore_SSE_AVX $(( 1 * 2**30 )) 10 | cut -d';' -f1,9 > ntstore_DRAM_1.dat
-# Running "ntstore" benchmark on 2 GiB of memoy
-taskset -c 2 ./ntstore_SSE_AVX $(( 2 * 2**30 )) 10 | cut -d';' -f1,9 > ntstore_DRAM_2.dat
-# Drawing the store benchmark plot
-cd ..
-gnuplot -c "plot_nstore_bw.gp" > ntstore_bw.png
-
-
-# TODO: pc benchmark
-# TODO: reduc benchmark
-# TODO: dotprod benchmark
-# TODO: triad benchmark
-
+if [ $# -gt 0 ]; then
+	for bench in $@; do
+		case $bench in
+			"load")
+				load_benchmark;;
+			"store")
+				store_benchmark;;
+			"ntstore")
+				ntstore_benchmark;;
+			"copy")
+				copy_benchmark;;
+			"memcpy")
+				memcpy_benchmark;;
+			"pc")
+				pc_benchmark;;
+			"reduc")
+				reduc_benchmark;;
+			"dotprod")
+				dp_benchmark;;
+			"triad")
+				triad_benchmark;;
+			"all")
+				load_benchmark
+				store_benchmark
+				ntstore_benchmark
+				copy_benchmark
+				memcpy_benchmark
+				pc_benchmark
+				reduc_benchmark
+				dp_benchmark
+				triad_benchmark	;;
+			*)
+				echo "wrong benchmark name!";;
+		esac
+	done
+else
+	echo "usage: sudo sh bash.sh [all | bechmark_names..]"
+fi
